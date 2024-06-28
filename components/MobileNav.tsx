cfc96c4aa8a49262c17085feeeb80e46a8a2c665
VigneshKannaiyan <kannavignesh01.tq@gmail.com>
2024-06-29 03:02:19 +0530

    navbar and mobile navbar changes

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,5 +1,6 @@
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 interface Props {
   nav: boolean;
@@ -14,6 +15,27 @@ const MobileNav = ({ nav, closeNav }: Props) => {
       className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}
     >
       <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+        <div className="relative inset-0 top-0 flex items-center justify-center gap-8 mb-8">
+          <a
+            href="https://www.instagram.com/coimbatore_columbus/"
+            target="_blank"
+          >
+            <FaInstagram className="w-12 h-12 text-[#f552f2]" />
+          </a>
+          <a href="https://github.com/VigneshKannaiyan" target="_blank">
+            <FaGithub className="w-12 h-12 text-[#ffffff]" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/vignesh-k-b40ab718b/"
+            target="_blank"
+          >
+            <FaLinkedin className="w-12 h-12 text-[#5fbfec]" />
+          </a>
+          <a href="https://wa.me/9952209682" target="_blank">
+            <FaWhatsapp className="w-12 h-12 text-[#40f381]" />
+          </a>
+        </div>
         <div className="nav-link-mobile">
           <a href="#home">HOME</a>
         </div>
